fe736fd17ff3003a38d66d4f584fc1ee99174545
Taylor Roemer <taylorcoleroemer18@gmail.com>
2022-09-02 21:37:00 -0500

    first commit

---
+++ server.js
@@ -0,0 +1,42 @@
+const express = require("express");
+const morgan = require("morgan");
+const bodyParser = require("body-parser");
+const bcrypt = require("bcrypt-nodejs");
+const cors = require("cors");
+const knex = require("knex");
+
+const { handleRegister } =require("./controllers/register");
+const { handleSignIn } = require("./controllers/signin");
+const { handleProfile } = require("./controllers/profile");
+const { handleImage, handleApiCall } = require("./controllers/image");
+
+const db = knex({
+  client: "pg",
+  connection: {
+    host: "127.0.0.1",
+    user: "taylor_roemer",
+    password: "",
+    database: "smart-brain",
+  },
+});
+
+const app = express();
+app.use(morgan("dev"));
+app.use(bodyParser.json());
+app.use(cors());
+
+app.get("/", (req, res) => { res.send('success') });
+
+app.post("/signin", handleSignIn(db, bcrypt));
+
+app.post("/register", handleRegister(db, bcrypt));
+
+app.get("/profile/:id", handleProfile(db));
+
+app.put("/image", handleImage(db));
+
+app.post("/imageurl", (req, res) => { handleApiCall(req, res) });
+
+app.listen(3000, () => {
+  console.log("App is running on port 3000");
+});
